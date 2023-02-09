2dc078160fd5d6d4679a13071d4332937566ce07
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-02-09 15:16:28 -0500

    Part 7 - Customizing Admin Form Completed

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline): # StackedInline
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,6 +3,8 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+from django.contrib import admin
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,6 +12,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now # Setting a pub_date in the future should mean that the Question is published at that moment, but invisible until then
