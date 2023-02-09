7ab99c6b5cdf807e9e13047f0114cf9e8ec8ce1f
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-02-09 01:10:06 -0500

    Part 5 halfway complete, Resume at Test a view

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,7 +11,9 @@ class Question(models.Model):
         return self.question_text
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1) #Bug
 
 
 class Choice(models.Model):
